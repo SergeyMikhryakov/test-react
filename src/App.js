ee399215d2b01acdf9d189e3232c0af09b36b25e
mihrjakovsv <mihrjakovsv@gmail.com>
2020-02-26 08:55:22 +0500

    Динамический import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>Clear</div>
-  );
+class App extends React.Component {
+  state = {
+    c: 0
+  }
+
+  componentDidMount() {
+    this.add(16, 49);
+  }
+
+  add = (a, b) => {
+    // Динамический import можно использовать даже внутри функции
+    import("./math").then(math => {
+      this.setState({c: math.add(a, b)});
+    });
+  }
+
+  render() {
+
+    return (
+        <div>Sum = {this.state.c}</div>
+    );
+  }
 }
 
 export default App;
